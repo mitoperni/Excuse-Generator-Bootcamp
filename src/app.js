57f6bdb5e625a634a2fec62261ea4f8496ece1c3
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-06-19 13:38:38 +0000

    Ejercicio completado como se pide

--- src/app.js
+++ src/app.js
@@ -5,7 +5,58 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const who = [
+  "El programador junior",
+  "Mi gato",
+  "El servidor",
+  "El repositorio de GitHub",
+  "El hacker",
+  "El IDE",
+  "El tester",
+  "El bot de CI/CD"
+];
+const action = [
+  "borró",
+  "crasheó",
+  "compiló mal",
+  "falló en la integración",
+  "rompió",
+  "eliminó",
+  "sobreescribió",
+  "corrompió"
+];
+const what = [
+  "el código fuente",
+  "la base de datos",
+  "los tests unitarios",
+  "el commit",
+  "la rama principal",
+  "el pull request",
+  "la documentación",
+  "el script de despliegue"
+];
+const when = [
+  "hace un momento",
+  "anoche",
+  "durante el fin de semana",
+  "esta mañana",
+  "durante la última build",
+  "ayer por la tarde",
+  "mientras dormía",
+  "en el último sprint"
+];
+
+const getRandomNumber = arr => arr[Math.floor(Math.random() * arr.length)];
+
+const dataset = [who, action, what, when];
+
+const excuseDataset = () =>
+  (document.querySelector("#excuse").innerHTML = dataset
+    .map(el => getRandomNumber(el))
+    .join()
+    .replaceAll(",", " "));
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  excuseDataset();
 };
