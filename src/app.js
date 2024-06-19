713f0f276b16d14da2d471e68f7760487108b2f2
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-06-19 17:49:52 +0000

    Guardado de seguridad para el botón

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+/* Mi codigo empieza aquí */
+
 const who = [
   "El programador junior",
   "Mi gato",
@@ -13,7 +15,7 @@ const who = [
   "El hacker",
   "El IDE",
   "El tester",
-  "El bot de CI/CD"
+  "El bot de CI/CD",
 ];
 const action = [
   "borró",
@@ -23,7 +25,7 @@ const action = [
   "rompió",
   "eliminó",
   "sobreescribió",
-  "corrompió"
+  "corrompió",
 ];
 const what = [
   "el código fuente",
@@ -33,7 +35,7 @@ const what = [
   "la rama principal",
   "el pull request",
   "la documentación",
-  "el script de despliegue"
+  "el script de despliegue",
 ];
 const when = [
   "hace un momento",
@@ -43,24 +45,42 @@ const when = [
   "durante la última build",
   "ayer por la tarde",
   "mientras dormía",
-  "en el último sprint"
+  "en el último sprint",
 ];
 
-const getRandomNumber = arr => arr[Math.floor(Math.random() * arr.length)];
+const getRandomNumber = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 const dataset = [who, action, what, when];
 
-const excuseDataset = () =>
-  (document.querySelector("#excuse").innerHTML = dataset
-    .map(el => getRandomNumber(el))
+let counter = 0;
+
+const excuseDataset = () => {
+  document.querySelector("#excuse").innerHTML = dataset
+    .map((el) => getRandomNumber(el))
     .join()
-    .replaceAll(",", " "));
+    .replaceAll(",", " ");
+};
 
 document
   .querySelector("#getNewExcuse")
   .addEventListener("click", excuseDataset);
 
+
 window.onload = function() {
-  //write your code here
   excuseDataset();
 };
+
+let textCounterExceded =
+  "Has excedido el número máximo de excusas, debes quedarte con esta!!";
+
+const counterExceded = () => {
+  if (counter > 10) {
+    document
+      .querySelector("#getNewExcuse")
+      .classList.remove("btn-outline-success");
+    document.querySelector("#getNewExcuse").classList.add("btn-danger");
+    document.querySelector("#getNewExcuse").classList.add("disabled");
+    document.querySelector("#getNewExcuse").innerHTML = textCounterExceded;
+  }
+};
+
